fdec5e2b7d5e5081f3d0b971cbd1c4e0d27dd2e5
edmoreno8704 <edison.capera12@gmail.com>
2023-05-16 08:44:53 -0500

    cambios en el archivo de admin para la vista Voto

--- votaciones/apps/votar/admin.py
+++ votaciones/apps/votar/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 
 # Register your models here.
+
+
+class VotarAdmin(admin.ModelAdmin):
+    search_fields = ['no_documento_votante']
+    list_display = ('no_documento_votante',
+                    'no_documento_candidato', 'estado', 'fecha_creacion', )
+
+
+admin.site.register(Voto, VotarAdmin)
